1310a87074085035dd589ee7321f7e7df3d79d4b
Serinaanto <serinaanto07@gmail.com>
2026-08-20 14:54:43 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def linear_search(arr, target):
-    for idx, value in enumerate(arr):
-        if value == target:
-            return idx
-    return -1
-
-print(linear_search([10, 20, 30, 40, 50], 40))
